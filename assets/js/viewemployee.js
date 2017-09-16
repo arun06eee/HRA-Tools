2e06f4ea69fc3bd21ac8036df33ecc1c2fbd4c9b
Arun kumar <arun06eee@gmail.com>
2017-09-16 17:12:52 +0530

    View employee modal

--- assets/js/viewemployee.js
+++ assets/js/viewemployee.js
@@ -12,6 +12,24 @@ $(function() {
 				$('.invalid').remove();
 			});
 		}
+        
+    $(".next-step").click(function (e) {
+        var $active = $('.wizard .nav-tabs li.active');
+        $active.next().removeClass('disabled');
+        nextTab($active);
+    });
+    
+    $(".prev-step").click(function (e) {
+        var $active = $('.wizard .nav-tabs li.active');
+        prevTab($active);
+    });
+
+    function nextTab(elem) {
+        $(elem).next().find('a[data-toggle="tab"]').click();
+    }
+    function prevTab(elem) {
+        $(elem).prev().find('a[data-toggle="tab"]').click();
+    }
 
 		ViewEmployee.prototype.initialization = function() {
 			return (function(instance) {
